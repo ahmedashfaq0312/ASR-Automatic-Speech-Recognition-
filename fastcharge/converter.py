8900956ff82cd7103fa046e51d049461884b1ffa
Tobias <t.schafberger@oth-aw.de>
2024-01-08 15:39:39 +0100

    Added first version of FastCharge Dataset to repo.

---
+++ fastcharge/converter.py
@@ -0,0 +1,53 @@
+import os
+import mat73
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+class FastChargeConverter():
+    def __init__(self, fastcharge_root):
+        self.fastcharge_root = fastcharge_root
+        self.dataset_files = ["2017-05-12_batchdata_updated_struct_errorcorrect.mat", "2017-06-30_batchdata_updated_struct_errorcorrect.mat", "2018-02-20_batchdata_updated_struct_errorcorrect.mat"]
+        
+
+    def load_measurement(self, file):
+        """Loads mat file.
+        """
+        return mat73.loadmat(file)["batch"]
+    
+    def concat_and_save_data(self, data, out_file):
+        summary = data["summary"]
+        policy = data["policy"]
+        cycle_lifes = data["cycle_life"]
+        fastcharge_summary_df = pd.DataFrame([])
+        for i in range(len(summary)):
+            if policy[i] not in ["3_6C_80PER_3_6C_SLOWCYCLE", "4_8C_80PER_4_8C_SLOWCYCLE"]: # bad measurements
+                print(f"Processing {policy[i]}")
+                if np.isnan(cycle_lifes[i]):
+                    cycle_lifes[i] = -1
+                tmp_df = pd.DataFrame(summary[i])
+                tmp_df["Experiment"] = policy[i]
+                tmp_df["Cycle_Life"] = cycle_lifes[i]
+                fastcharge_summary_df = pd.concat([fastcharge_summary_df, tmp_df], ignore_index=True)
+        # drop zeros
+        # fastcharge_summary_df.loc[~(fastcharge_summary_df["Q"]==0).all(axis=1)]
+        fastcharge_summary_df.to_csv(out_file)
+
+    def convert(self):
+        """Converts all cell data.
+        """
+        for file in self.dataset_files:
+            print(f"Converting {file}")
+            if file == "2018-02-20_batchdata_updated_struct_errorcorrect.mat":
+                out_file = "FastCharge_201802.csv"
+            elif file == "2017-06-30_batchdata_updated_struct_errorcorrect.mat":
+                out_file = "FastCharge_201706.csv"
+            elif file == "2017-05-12_batchdata_updated_struct_errorcorrect.mat":
+                out_file = "FastCharge_201705.csv"
+            else:
+                out_file = "error.csv"
+            if not os.path.exists(f"{self.fastcharge_root}/{out_file}") and os.path.exists(f"{self.fastcharge_root}/{file}"):
+                data = self.load_measurement(file)
+                self.concat_and_save_data(data, out_file)
+            
+        
---
+++ fastcharge/dataset.py
@@ -0,0 +1,87 @@
+import os
+import pandas as pd
+import numpy as np
+from .converter import FastChargeConverter
+
+class FastChargeDataset():
+    def __init__(self, dataset_config):
+        self.capacities = {}
+        self.measurement_times = {}
+        self.sohs = {}
+
+        self.rated_capacity = 1.1
+        self.dataset_config = dataset_config
+        self.fastcharge_root = self.dataset_config.dataset_root_dir
+        self.batteries = self.dataset_config.battery_list
+        self.battery_cells = self.batteries
+        self.normalize = self.dataset_config.normalize_data
+        self.clean_data = self.dataset_config.clean_data
+        self.rated_capacity = self.dataset_config.rated_capacity
+        self.smooth_data = self.dataset_config.smooth_data
+        self.smoothing_kernel_width = self.dataset_config.smoothing_kernel_width
+        self.concat_data_file_times = ["201705", "201706", "201802"]
+
+        self.fastcharge_converter = FastChargeConverter(self.fastcharge_root)
+        self.load()
+        self.get_dataset_length()
+    
+    def get_positional_information(self):
+        """Extracts positional information from data.
+        """
+        self.positions = {}
+        for data_index, data in self.capacities.items():
+            data_length = len(data)
+            self.positions[data_index] = list(range(1, data_length+1))
+
+    def get_temporal_information(self):
+        """Extracts temporal information from data.
+        """
+        for cell_id in self.batteries:
+            self.measurement_times[cell_id] = self.normalize_data(self.measurement_times[cell_id])
+    
+    def get_dataset_length(self):
+        """Calculates the length of the loaded dataset.
+        """
+        self.dataset_length = 0
+        for caps in self.capacities.values():
+            self.dataset_length += len(caps)
+
+    def normalize_capacities(self):
+        """Normalizes capacities.
+        """
+        for cell_id, cap in self.capacities.items():
+            normalized_capacities = [i/self.rated_capacity for i in cap]
+            self.capacities[cell_id] = normalized_capacities
+
+    def smooth_capacities(self):
+        box = np.ones(self.smoothing_kernel_width) / self.smoothing_kernel_width
+        box_pts_half = self.smoothing_kernel_width // 2
+        for cell_id, cap in self.capacities.items():
+            cap_smooth = np.convolve(cap, box, mode="same").flatten().tolist()
+            # remove very different values at start and end
+            cap_smooth[:box_pts_half] = cap[:box_pts_half]
+            cap_smooth[-box_pts_half:] = cap[-box_pts_half:]
+            self.capacities[cell_id] = cap_smooth
+            # self.capacities[f"{cell_id}_smoothed"] = cap_smooth
+        return cap_smooth
+
+    def load(self):
+        """Loads all cycling data for FastCharge dataset.
+        """
+        self.fastcharge_converter.convert()
+        
+        for file_time in self.concat_data_file_times:
+            file = f"{self.fastcharge_root}/FastCharge_{file_time}.csv"
+            if os.path.exists(file):
+                data = pd.read_csv(file)
+                experiments = data["Experiment"].unique().tolist()
+                for experiment in experiments:
+                    tmp_data = data[data["Experiment"] == experiment]
+                    self.capacities[f"{file_time}_{experiment}"] = tmp_data["QDischarge"].tolist()
+            else:
+                print(f"Path {file} does not exist")
+        if self.normalize:
+            self.normalize_capacities()
+
+
+
